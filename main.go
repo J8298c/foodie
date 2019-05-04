81c35b3aa8f486095c7210753b859d0e814460dc
j8298c <juliocmojica@gmail.com>
2019-05-04 10:09:54 -0400

    handleers for methods

--- main.go
+++ main.go
@@ -22,25 +22,31 @@ func getAllVenues(w http.ResponseWriter, r *http.Request) {
 
 func getSingleVenue(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("Getting a single venue")
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "id: %v\n", vars["id"])
 }
 
-func createVenue() {
+func createVenue(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("creating a venue")
 }
 
-func addDishToVenue() {
+func addDishToVenue(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("add a dish to the venue")
 }
 
-func editVenue() {
+func editVenue(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("edit a venue")
 }
 
 func main() {
 	fmt.Print("hello")
 	r := mux.NewRouter()
-	r.HandleFunc("/", index)
-	r.HandleFunc("/venues/all", getAllVenues)
-	r.HandleFunc("/venues/{id}", getSingleVenue)
+	r.HandleFunc("/", index).Methods("GET")
+	r.HandleFunc("/venues/all", getAllVenues).Methods("GET")
+	r.HandleFunc("/venues/{id}", getSingleVenue).Methods("GET")
+	r.HandleFunc("/venues/new", createVenue).Methods("POST")
+	r.HandleFunc("/venues/{venue}/dish", addDishToVenue).Methods("POST")
+	r.HandleFunc("/venues/{venue}/edit", editVenue).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
