2b8cd7a58daa9597212e9a549fff16c17a790d40
j8298c <juliocmojica@gmail.com>
2019-05-04 13:20:24 -0400

    connecting to db

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Venue defines struct for venues
@@ -36,6 +41,30 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func getAllVenues(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("Getting all venue")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
+	collection := client.Database("yumgum").Collection("restaurants")
+	cur, err := collection.Find(ctx, bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cur.Close(ctx)
+
+	for cur.Next(ctx) {
+		var result bson.M
+		err := cur.Decode(&result)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, "results: %v\n", result)
+	}
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func getSingleVenue(w http.ResponseWriter, r *http.Request) {
