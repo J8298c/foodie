cc2849c6b8b2fcbad4f1ba887b8d908be54d12bc
j8298c <juliocmojica@gmail.com>
2019-05-08 06:58:56 -0400

    go server

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Venue defines struct for venues
@@ -40,50 +35,12 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func getAllVenues(w http.ResponseWriter, r *http.Request) {
-	fmt.Print("Getting all venue")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		panic(err)
-	}
-	collection := client.Database("yumgum").Collection("venues")
-	cur, err := collection.Find(ctx, bson.D{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer cur.Close(ctx)
-
-	for cur.Next(ctx) {
-		var result bson.M
-		err := cur.Decode(&result)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Fprintf(w, "results: %v\n", result)
-	}
-	if err := cur.Err(); err != nil {
-		log.Fatal(err)
-	}
+	//TODO: share mongo connection dont do it per request
+	fmt.Print("fetching all venues")
 }
 
 func getSingleVenue(w http.ResponseWriter, r *http.Request) {
-	fmt.Print("Getting a single venue")
-	vars := mux.Vars(r)
-	w.WriteHeader(http.StatusOK)
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		panic(err)
-	}
-	filter := bson.M{"_id": vars["id"]}
-	var results bson.M
-	collection := client.Database("yumgum").Collection("venues")
-	err = collection.FindOne(ctx, filter).Decode(&results)
-	if err != nil {
-		fmt.Print(err)
-	}
-	fmt.Fprintf(w, "id: %v\n", results)
+	fmt.Print("fetching a venues")
 }
 
 func createVenue(w http.ResponseWriter, r *http.Request) {
